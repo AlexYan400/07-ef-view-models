7b5d773c8409fb1c107104de32b9016c44d6a01b
Dr. Brian Bansenauer <brianb@cascadia.edu>
2026-02-12 12:52:52 -0800

    Initial commit

---
+++ LuckySpin/ViewModels/PlayersChoice.cs
@@ -0,0 +1,10 @@
+using LuckySpin.Models;
+namespace LuckySpin.ViewModels
+{
+    public class PlayersChoice
+    {
+        public Player Player { get; set; }
+        //TODO: Add  additional properties needed to pre-fill the PlayersChoice View.
+
+    }
+}
